95028f855b63e7126faa6d995cd29646029d136b
GwerhDev <gwerh.dev@gmail.com>
2024-04-04 16:52:26 -0300

    methods & testing: methods listIcon & getIcon and tests passed

--- tests/listIcon.spec.js
+++ tests/listIcon.spec.js
@@ -2,6 +2,5 @@ const { listIcon } = require('../');
 
 test('La función listIcon devuelve un array', () => {
   const result = listIcon();
-  console.log(result)
   expect(Array.isArray(result)).toBe(true);
 });
